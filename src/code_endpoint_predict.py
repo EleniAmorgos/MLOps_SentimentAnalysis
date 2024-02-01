c2937df4a707104437693bd7e1c75e8f8b4ce72c
EleniAmorgos <heleneassir@yahoo.fr>
2024-02-01 14:53:31 +0100

    LN : màj predictions 2

--- src/code_endpoint_predict.py
+++ src/code_endpoint_predict.py
@@ -9,8 +9,8 @@ print(sklearn.__version__)
 # Vectorization du commentaire pour les modèles de Deep Learning
 comment_DL = "génial super cool adore aime"
 def predict(comment):
-    vectorizer_fit = pickle.load(open('model/src/vect_dtc.pkl', 'rb'))
-    model = pickle.load(open('model/src/model_dtc.pkl', 'rb'))
+    vectorizer_fit = pickle.load(open('predictions/vect_dtc.pkl', 'rb'))
+    model = pickle.load(open('predictions/model_dtc.pkl', 'rb'))
     pred = model.predict(vectorizer_fit.transform([comment]))[0]
     proba = model.predict_proba(vectorizer_fit.transform([comment]))[0]
     return ("predicted class:", pred, 'probabilité de négatif et positif:', proba)
