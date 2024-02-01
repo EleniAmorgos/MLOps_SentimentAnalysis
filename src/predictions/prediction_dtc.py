54aa28389bafbcfe3ffaea262fcbb669594a99ff
EleniAmorgos <heleneassir@yahoo.fr>
2024-02-01 14:51:35 +0100

    LN : màj pour prediction

---
+++ src/predictions/prediction_dtc.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan  9 00:30:02 2023
+
+@author: t.fourtouill
+"""
+
+import numpy as np
+import pickle
+from sklearn.tree import DecisionTreeClassifier
+
+
+comment = 'comment transmis par streamlit dans les params'
+
+def predict(comment):
+    vectorizer_fit = pickle.load(open('files/vec_dtc.pickle', 'rb'))
+    model = pickle.load(open('files/model_dtc.pickle', 'rb'))
+
+    pred = model.predict(vectorizer_fit.transform([comment]))[0]
+    
+    return ("predicted class:", pred)
